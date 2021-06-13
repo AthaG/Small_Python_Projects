87e89f9f4e58f21606ebc31bf7adb43a52ab9a2c
AthaG <52504599+AthaG@users.noreply.github.com>
2021-06-13 20:42:20 +0200

    Changed name

--- SquareRootOfTrinomials.py
+++ SquareRootOfTrinomials.py
@@ -1,12 +1,16 @@
-# Square-Root-of-Trinomials
-
+# Square-Root of Trinomials
 import math
+
+
 print("Έχουμε ένα τριώνυμο ax²+bx+c. Δώστε μία θετική ή αρνητική τιμή σε κάθε σταθερά!")
 a=int(input("a:"))
 b=int(input("b:"))
 c=int(input("c:"))
+
 D= b**2-4*a*c
+
 print("Η Διακρίνουσα ειναι: " + str(D))
+
 if D>0:
     x1=(-b+math.sqrt(D))/(2*a)
     print("Η πρώτη ρίζα ειναι: " + str(x1))
